d7f6005db0532ef3c55bf2b5ad6591411c9a64ad
Kyle Richnafsky <7krich@gmail.com>
2022-04-16 15:21:12 -0400

    set up server file

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+// init express application
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// require the route.js file
+require('./routes/routes')(app);
+
+// include css & javascript files when HTML file loads in get send file route below
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// route listener
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
