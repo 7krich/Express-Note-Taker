e680c4adaef79edacd9e5ed7ae120f8d855f3850
Kyle Richnafsky <7krich@gmail.com>
2022-04-16 16:12:54 -0400

    flipped from router to app for route calls and ID calls

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,20 +2,33 @@ const fs = require('fs');
 const path = require('path');
 
 // get notes route
-router.get("/api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
     // return saved notes in db.json file
     res.json(notes);
 });
 
 // post notes route
-router.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
     // new note = note input body
     let newNotes = req.body;
     notes.push(newNotes);
     // call add to db function to add new notes to db.json
-    addDb();
+    editDb();
     return console.info(`Added new note: ${newNotes.title}`);
 });
+
+// get note by ID
+app.get("/api/notes/:id", (req, res) => {
+    // display json for note array for given ID
+    res.json(notes[req.params.id])
+});
+
+// Delete notes by ID
+app.delete("/api/notes/:id", (req, res) => {
+    notes.splice(req.params.id, 1);
+    editDb();
+    console.log(`Note id: ${req.params.id} deleted.`)
+})
 
 
 
