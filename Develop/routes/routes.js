2dda169ed0cfcc6db95e5346ee35528fd8e993e7
Kyle Richnafsky <7krich@gmail.com>
2022-04-18 18:22:49 -0400

    notes now save and re-populate

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -3,17 +3,26 @@ const fs = require('fs');
 const path = require('path');
 let { notes } = require('../db/db.json');
 
+// API Routes
 // get notes api route
 router.get("/api/notes", (req, res) => {
     // return saved notes in db.json file
     res.json(notes);
 });
 
-// API Routes
-// post notes api route
+// post notes api route & allow note retrieval
 router.post("/api/notes", (req, res) => {
     // new note = note input body
     let newNotes = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    // add ID to new notes in the list
+    newNotes.id = notelength;
+
+    // add updated note to db.json file
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
 
     // check if there is an array, if not, create one
     if (!Array.isArray(notes)) {
@@ -21,8 +30,6 @@ router.post("/api/notes", (req, res) => {
     }
     // then push the new notes to the array
     notes.push(newNotes);
-    // call add to db function to add new notes to db.json
-    editDb();
     return console.log(`Added new note: ${newNotes.title}`);
 });
 
@@ -35,7 +42,6 @@ router.get("/api/notes/:id", (req, res) => {
 // Delete notes by ID
 router.delete("/api/notes/:id", (req, res) => {
     notes.splice(req.params.id, 1);
-    editDb();
     console.log(`Note id: ${req.params.id} deleted.`)
 });
 
@@ -50,15 +56,6 @@ router.get('/notes', (req, res) => {
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
-// Functionality
-// adds or deletes json objects when a note is added or deleted
-function editDb() {
-    fs.writeFile("db/db.json", JSON.stringify(notes,'\t'), err => {
-        if (err) throw err;
-        return true;
-    });
-};
 
 module.exports = router;
 
